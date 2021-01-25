2be95157c720ba4a66574e612e9c54c3cce544ce
jstloyal <jstloyalty@gmail.com>
2021-01-25 07:10:52 +0100

    add card component to request page

--- src/pages/Requests.js
+++ src/pages/Requests.js
@@ -1,12 +1,73 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, Card, Badge, DropdownButton, SplitButton, Dropdown } from 'react-bootstrap';
+import ExpandMoreRoundedIcon from '@material-ui/icons/ExpandMoreRounded';
+import { Link } from 'react-router-dom';
 
 function Requests() {
   return (
     <div className="content">
-      <Button>Request Information</Button>
+      <Button size="sm" className="my-btn">Request Information</Button>
+      <Card className="mt-4 my-card">
+        <div className="card-info pb-3">
+          <div className="d-flex flex-md-column col">
+            <div className="d-flex flex-md-column p-2">
+              <p className="card-text-head mb-1">REQUEST TYPE</p>
+              <p className="card-text">POST-CON</p>
+            </div>
+            <div className="d-flex flex-md-column p-2">
+              <p className="card-text-head mb-1">INVOICE STATUS</p>
+              <Badge className="my-badge">UNGENERATED</Badge>
+            </div>
+            <div className="d-flex flex-md-column p-2">
+              <p className="card-text-head mb-1">LOCATION</p>
+              <p className="card-text">8, YOVI STREET, IWAYA, ONIKE, YABA</p>
+            </div>
+          </div>
+          <div className="d-flex flex-md-column col">
+            <div className="d-flex flex-md-column p-2">
+              <p className="card-text-head mb-1">CUSTOMER NAME</p>
+              <p className="card-text">PETER ABU-EKPESHIE</p>
+            </div>
+            <div className="d-flex flex-md-column p-2">
+              <p className="card-text-head mb-1">REQUEST DATE</p>
+              <p className="card-text">06-10-2016</p>
+            </div>
+            <div className="d-flex flex-md-column p-2">
+              <p className="card-text-head mb-1">HOUSE-TYPE</p>
+              <p className="card-text">5-BEDROOM DUPLEX</p>
+            </div>
+          </div>
+          <div className="d-flex flex-md-column col">
+            <div className="d-flex flex-md-column p-2">
+              <p className="card-text-head mb-1">REQUEST TYPE</p>
+              <p className="card-text">POST-CON</p>
+            </div>
+            <div className="d-flex flex-md-column p-2">
+              <p className="card-text-head mb-1">INVOICE STATUS</p>
+              <p className="card-text">UNGENERATED</p>
+            </div>
+            <div className="d-flex flex-md-column p-2">
+              <p className="card-text-head mb-1">LOCATION</p>
+              <p className="card-text">8, YOVI STREET, IWAYA, ONIKE, YABA</p>
+            </div>
+          </div>
+        </div>
+        <div className="mt-4">
+          <SplitButton
+            menuAlign={{ lg: 'left'}}
+            title="Actions"
+            id="dropdown-menu-align-responsive-2"
+            drop="up"
+            className="my-btn actions float-right"
+          >
+            <Dropdown.Item eventKey="1">Contact customer</Dropdown.Item>
+            <Dropdown.Item eventKey="2">Generate invoice</Dropdown.Item>
+            <Dropdown.Item eventKey="3">Reject request</Dropdown.Item>
+          </SplitButton>
+        </div>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
 export default Requests;
